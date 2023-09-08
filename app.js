cbbf87ba071ff14804801396f4155ed4233c90ce
TRoYHD <dark_angle.2010@hotmail.com>
2023-09-09 01:37:13 +0300

    dasdasdasdasd

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
+// Imports
 import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
-import { fileURLToPath } from 'url'; // Import fileURLToPath
-import { dirname } from 'path'; // Import dirname
-import bookRoutes from "./routes/bookRoutes.js";
+import bookRoutes from "./routes/bookRoutes.js"
 
-const app = express();
+
+const app = express() ;
 
 // defining the port for the application to listen
-const port = 3000;
+const port = 3000
 
 // Added Application Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 
 // defining the template engine for the application
-app.set('view engine', 'pug');
-
-// Use fileURLToPath and dirname to obtain the current directory
-const __filename = fileURLToPath(import.meta.url);
-const currentDir = dirname(__filename); // Use a different variable name, e.g., currentDir
+app.set('view engine' , 'pug');
 
 // defining the views file for the template engine
-app.set('views', path.resolve(currentDir, 'dist'));
+app.set('views' , path.resolve('./dist'));
 
-app.get('/', (req, res) => {
-  const filePath = path.resolve(currentDir, 'data', 'books.json');
-  res.sendFile(filePath);
+
+app.get('/' , (req , res) => {
+
+res.redirect('/books')
 });
+
 
 // Added Application Middleware
 app.use(bookRoutes);
@@ -37,6 +35,10 @@ app.use((req, res, next) => {
   res.status(404).render("404", { pageTitle: "Page not found!" });
 });
 
-app.listen(port, () => {
-  console.log('Express app is listening to port 3000!');
-});
+
+app.listen(port ,() => {
+
+console.log('Express app is listening to port 3000!');
+
+
+})
--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,17 +1,15 @@
 import fs from 'fs-extra';
-import path from 'path'; // Import the path module
 
 const getAllBooks = async (req, res) => {
   try {
-    const booksFilePath = path.join(__dirname, 'data', 'books.json'); // Construct the absolute path to 'data/books.json'
-    const books = await fs.readJson(booksFilePath);
+    const books = await fs.readJson('books.json');
 
     res.render('books', { books });
   } catch (error) {
     res.status(404).render('404', {
-      title: 'There is no file with the name books.json or the file is empty',
+      title: 'There is no file with name books.json or file is empty',
       url: '/add-book',
-      text: 'Create a book'
+      text: 'Create book'
     });
   }
 };
@@ -19,8 +17,7 @@ const getAllBooks = async (req, res) => {
 const getBookDetails = async (req, res) => {
   const { id } = req.params;
   try {
-    const booksFilePath = path.join(__dirname, 'data', 'books.json'); // Construct the absolute path to 'data/books.json'
-    const books = await fs.readJson(booksFilePath);
+    const books = await fs.readJson('books.json');
     const book = books.find(book => book.id === Number(id));
 
     if (!book) {
@@ -34,7 +31,7 @@ const getBookDetails = async (req, res) => {
     }
   } catch (error) {
     res.status(404).render('404', {
-      title: `Something went wrong!`,
+      title: `Something wrong!`,
       url: '/books',
       text: 'Go to books'
     });
@@ -45,15 +42,14 @@ const createBook = async (req, res) => {
   const { name } = req.body;
 
   try {
-    const booksFilePath = path.join(__dirname, 'data', 'books.json'); // Construct the absolute path to 'data/books.json'
-    const booksFileExists = await fs.pathExists(booksFilePath);
+    const booksFileExists = await fs.pathExists('books.json');
 
     if (!booksFileExists) {
-      await fs.ensureFile(booksFilePath);
-      await fs.writeJson(booksFilePath, [{ id: 1, name }]);
+      await fs.ensureFile('books.json');
+      await fs.writeJson('books.json', [{ id: 1, name }]);
       res.redirect('/books');
     } else {
-      const books = await fs.readJson(booksFilePath);
+      const books = await fs.readJson('books.json');
       const book = books.find(book => book.name === name);
 
       if (book) {
@@ -66,12 +62,12 @@ const createBook = async (req, res) => {
           name
         };
         books.push(newBook);
-        await fs.writeJson(booksFilePath, books);
+        await fs.writeJson('books.json', books);
         res.redirect('/books');
       }
     }
   } catch (error) {
-    if (error) await fs.writeJson(booksFilePath, [{ id: 1, name }]);
+    if (error) await fs.writeJson('books.json', [{ id: 1, name }]);
     res.redirect('/books');
   }
 };
